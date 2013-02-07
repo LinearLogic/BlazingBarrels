c55c5549a29437e639db3cfa2bc5ac9b945a2ffa
Jared Heath <silkrawnstudios@gmail.com>
2013-02-06 19:18:10 -0800

    [0.1.6] Add Camera interface (to be implemented
    by dimension-specific camera classes)

---
+++ src/com/veltro/blazingbarrels/engine/graphics/Camera.java
@@ -0,0 +1,53 @@
+package com.veltro.blazingbarrels.engine.graphics;
+
+import com.veltro.blazingbarrels.game.location.Location;
+
+/**
+ * Highest-order Camera interface - specifies three basic methods for updating the camera
+ * 
+ * @author LinearLogic
+ * @since 0.1.6
+ */
+public interface Camera<T extends Location> {
+
+	/**
+	 * Sets the screen's viewing window to that of the camera
+	 */
+	void useView();
+
+	/**
+	 * Detects input from the mouse and updates the Camera (its position, rotation, etc.) accordingly
+	 */
+	void handleMouseInput();
+
+	/**
+	 * Detects input from the keyboard and updates the Camera (its position, rotation, etc.) accordingly
+	 */
+	void handleKeyboardInput();
+
+	/**
+	 * Updates the Camera's position (read: the viewing window) based on the input processed in the
+	 * {@link #handleMouseInput()} and {@link #handleKeyboardInput()} methods.
+	 */
+	void updatePosition();
+
+	/**
+	 * Sets the camera's position to the provided pixel coordinates (within the game world, not the game window)
+	 * 
+	 * @param coordinates An Array of floating point values (the number of values depends on the dimensionality of the
+	 * camera, which is determined by the dimensionality of the {@link Location} subclass used for the camera
+	 */
+	void setPixelPosition(float... coordinates);
+
+	/**
+	 * @return The location of the camera (a {@link Location} subclass)
+	 */
+	T getPosition();
+
+	/**
+	 * Sets the position of the camera to the provided location
+	 * 
+	 * @param location A {@link Location} subclass
+	 */
+	void setPosition(T location);
+}
