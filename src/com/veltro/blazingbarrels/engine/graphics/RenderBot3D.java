26e55036c5140491764c3dd48c6da1bedf186bb1
Jared Heath <silkrawnstudios@gmail.com>
2013-04-03 04:15:10 -0400

    [0.4.9] Simplify Renderbot3D calls

--- src/com/veltro/blazingbarrels/engine/graphics/RenderBot3D.java
+++ src/com/veltro/blazingbarrels/engine/graphics/RenderBot3D.java
@@ -20,28 +20,6 @@ import com.veltro.blazingbarrels.game.location.Location3D;
 public class RenderBot3D {
 
 	/**
-	 * Renders a fully opaque cylinder with the specified attributes at the provided {@link Location3D location}. If
-	 * the bottom and top radii are the same, a normal cylinder will be drawn. If the two radii differ, a frustum (if
-	 * neither radius is zero) or cone (if one of the radii is zero) will be drawn.
-	 * 
-	 * @param bottomRadius The radius, in pixels, of the bottom of the cylinder
-	 * @param topRadius The radius, in pixels, of the top of the cylinder
-	 * @param height The height, in pixels, of the cylinder (the distance between its bases)
-	 * @param slices The number of edges on the circle around the sphere's equator (the more slices, the smoother the
-	 * surface of the sphere)
-	 * @param stacks The number of horizontal stacks to subdivide the sphere into (the more stacks, the less obvious
-	 * the horizontal segmentation of the sphere)
-	 * @param location The location (position and rotation) of the center of the base of the cylinder
-	 * @param r Red intensity (must be between 0.0 and 1.0, inclusive)
-	 * @param g Green intensity (must be between 0.0 and 1.0, inclusive)
-	 * @param b Blue intensity (must be between 0.0 and 1.0, inclusive)
-	 */
-	public void renderColoredCylinder(float bottomRadius, float topRadius, float height, int slices, int stacks,
-			Location3D location, float r, float g, float b) {
-		renderTransparentColoredCylinder(bottomRadius, topRadius, height, slices, stacks, location, r, g, b, 1);
-	}
-
-	/**
 	 * Renders a (partially) transparent cylinder with the specified attributes at the provided
 	 * {@link Location3D location}. If the bottom and top radii are the same, a normal cylinder will be drawn. If the
 	 * two radii differ, a frustum (if neither radius is zero) or cone (if one of the radii is zero) will be drawn.
@@ -59,7 +37,7 @@ public class RenderBot3D {
 	 * @param b Blue intensity (must be between 0.0 and 1.0, inclusive)
 	 * @param transparency The transparency factor of the rectangle (0.0 = entirely transparent, 1.0 = entirely opaque)
 	 */
-	public void renderTransparentColoredCylinder(float bottomRadius, float topRadius, float height, int slices,
+	public void renderColoredCylinder(float bottomRadius, float topRadius, float height, int slices,
 			int stacks, Location3D location, float r, float g, float b, float transparency) {
 		glPushMatrix();
 		if (slices < 3) // Nothing will be rendered
@@ -78,26 +56,6 @@ public class RenderBot3D {
 	}
 
 	/**
-	 * Renders a fully opaque disk with the specified attributes at the given {@link Location3D location}. The outer
-	 * radius is self-explanatory; the inner radius is the radius of the hole in the disk. If it is zero, the disk will
-	 * be solid throughout. If the inner radius is greater than zero, however, the result will be a washer (a disk with
-	 * a hollow center).
-	 * 
-	 * @param outerRadius The outer radius of the disk, in pixels
-	 * @param innerRadius The inner radius of the disk, in pixels
-	 * @param slices The number of horizontal and vertical slices to use to divide the disk for rendering (the higher
-	 * this number, the smoother the edges of the disk)
-	 * @param location The location (position and rotation) of the center of the disk
-	 * @param r Red intensity (must be between 0.0 and 1.0, inclusive)
-	 * @param g Green intensity (must be between 0.0 and 1.0, inclusive)
-	 * @param b Blue intensity (must be between 0.0 and 1.0, inclusive)
-	 */
-	public void renderColoredDisk(float outerRadius, float innerRadius, int slices, Location3D location, float r,
-			float g, float b) {
-		renderTransparentColoredDisk(outerRadius, innerRadius, slices, location, r, g, b, 1);
-	}
-
-	/**
 	 * Renders a (partially) transparent disk with the specified attributes at the given {@link Location3D location}.
 	 * The outer radius is self-explanatory; the inner radius is the radius of the hole in the disk. If it is zero, the
 	 * disk will be solid throughout. If the inner radius is greater than zero, however, the result will be a washer (a
@@ -113,7 +71,7 @@ public class RenderBot3D {
 	 * @param b Blue intensity (must be between 0.0 and 1.0, inclusive)
 	 * @param transparency The transparency factor of the rectangle (0.0 = entirely transparent, 1.0 = entirely opaque)
 	 */
-	public void renderTransparentColoredDisk(float outerRadius, float innerRadius, int slices, Location3D location,
+	public void renderColoredDisk(float outerRadius, float innerRadius, int slices, Location3D location,
 			float r, float g, float b, float transparency) {
 		glPushMatrix();
 		if (slices < 3) // Nothing will be rendered
@@ -138,20 +96,7 @@ public class RenderBot3D {
 	 * @param location The location (position and rotation) in the game world at which to render the model
 	 */
 	public void renderModel(Model model, Location3D location) {
-		renderTransparentColoredModel(model, location, 0, 0, 0, 1);
-	}
-
-	/**
-	 * Renders a fully opaque {@link Model} at the specified {@link Location3D location} with the specified color
-	 * 
-	 * @param model The model whose VBO to retrieve and draw
-	 * @param location The location (position and rotation) in the game world at which to render the model
-	 * @param r Red intensity (must be between 0.0 and 1.0, inclusive)
-	 * @param g Green intensity (must be between 0.0 and 1.0, inclusive)
-	 * @param b Blue intensity (must be between 0.0 and 1.0, inclusive)
-	 */
-	public void renderColoredModel(Model model, Location3D location, float r, float g, float b) {
-		renderTransparentColoredModel(model, location, r, g, b, 1);
+		renderColoredModel(model, location, 0, 0, 0, 1);
 	}
 
 	/**
@@ -165,7 +110,7 @@ public class RenderBot3D {
 	 * @param b Blue intensity (must be between 0.0 and 1.0, inclusive)
 	 * @param transparency The transparency factor of the rectangle (0.0 = entirely transparent, 1.0 = entirely opaque)
 	 */
-	public void renderTransparentColoredModel(Model model, Location3D location, float r, float g, float b,
+	public void renderColoredModel(Model model, Location3D location, float r, float g, float b,
 			float transparency) {
 		glPushMatrix();
         glBindBuffer(GL_ARRAY_BUFFER, model.getVBOVertexHandle());
@@ -192,25 +137,6 @@ public class RenderBot3D {
 	}
 
 	/**
-	 * Renders a fully opaque sphere with the specified attributes at the provided location
-	 * 
-	 * @param radius The radius, in pixels, of the sphere
-	 * @param slices The number of edges on the circle around the sphere's equator (the more slices, the smoother the
-	 * surface of the sphere)
-	 * @param stacks The number of horizontal stacks to subdivide the sphere into (the more stacks, the less obvious
-	 * the horizontal segmentation of the sphere)
-	 * @param x The x-coordinate, in pixels, of the sphere's center
-	 * @param y The y-coordinate, in pixels, of the sphere's center
-	 * @param z The z-coordinate, in pixels, of the sphere's center
-	 * @param g Green intensity (must be between 0.0 and 1.0, inclusive)
-	 * @param g Green intensity (must be between 0.0 and 1.0, inclusive)
-	 * @param b Blue intensity (must be between 0.0 and 1.0, inclusive)
-	 */
-	public void renderColoredSphere(float radius, int slices, int stacks, float x, float y, float z, float r, float g,
-			float b) {
-		renderTransparentColoredSphere(radius, slices, stacks, x, y, z, r, g, b, 1);
-	}
-	/**
 	 * Renders a (partially) transparent sphere with the specified attributes at the provided location
 	 * 
 	 * @param radius The radius, in pixels, of the sphere
@@ -226,7 +152,7 @@ public class RenderBot3D {
 	 * @param b Blue intensity (must be between 0.0 and 1.0, inclusive)
 	 * @param transparency The transparency factor of the rectangle (0.0 = entirely transparent, 1.0 = entirely opaque)
 	 */
-	public void renderTransparentColoredSphere(float radius, int slices, int stacks, float x, float y, float z, float r,
+	public void renderColoredSphere(float radius, int slices, int stacks, float x, float y, float z, float r,
 			float g, float b, float transparency) {
 		glPushMatrix();
 		if (slices < 2)
